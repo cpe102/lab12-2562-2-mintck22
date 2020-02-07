1064efbeb22e48e5b574f0e94ac3fae69719f0b7
Mint <chawanluck_k@hotmail.com>
2020-02-07 16:58:55 +0700

    2

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include<iomanip>
 using namespace std;
 
 const int N = 30;
@@ -23,3 +24,52 @@ int main()
 }
 
 // Write definition of updateImage() and showImage() here
+void updateImage(bool image[][M],int s,int x,int y){
+
+     for (int i = 0; i <= N-1; i++)
+    {
+        for (int j = 0; j <= M-1; j++)
+        {
+            if(sqrt(pow(i-x,2)+pow(j-y,2)) <= s-1){
+                image[i][j] = 1;
+            }
+        }
+        
+    }
+}
+   
+
+void showImage(const bool image[][M]){
+    for (int k = 0; k <= M+1; k++)
+        {
+            cout << "-";
+        }
+    cout << "\n";
+    
+        
+    for (int i = 0; i <= N-1; i++)
+    {
+        cout << "|";
+        for (int j = 0; j <= M-1; j++)
+        {  
+            if (image[i][j] == 1){
+                cout << "*";}
+            else
+            {
+                cout << " ";
+            }
+            
+        }
+        cout << "|";
+        cout << "\n";
+    }
+
+    for (int k = 0; k <= M+1; k++)
+        {
+            cout << "-";
+        }
+    cout << "\n";
+    
+}
+
+   
